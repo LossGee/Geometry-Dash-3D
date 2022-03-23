fd07f353759e469e395cb376696279b967ae4b30
LossGee <asdfrv20@gmail.com>
2022-03-23 19:39:41 +0900

    Player(Cube) Jump Complete

--- Geometry_Dash_3D/Assets/Scripts/PlayerMove.cs
+++ Geometry_Dash_3D/Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,73 +12,126 @@ step3) Jump시 MotionCube의
  */
 public class PlayerMove : MonoBehaviour
 {
-    public float moveSpeed = 8f;
+    // Player mode
+    enum ModeState { 
+        CUBE,
+        ROCKET
+    }
+    ModeState Mode;
+
+
+    // 공통 변수
+    public float moveSpeed = 20f;        
     float gravity = -9.8f;
     float yVelocity = 0f;
-    public float jumpPower = 4f;
-    bool jumpState = false;
-    int junmTurn = 180;
-    float rot = 0f;
 
-    Vector3 dir = new Vector3(0, 0, 0);
+    Vector3 dir;
     CharacterController cc;
+
+    // Palyer(Cube) 변수
+    public float jumpPower = 2f;
+    bool jumpState = false;         // jump 상태(ture: 점프중 // false: 점프X) - 이단 점프 방지
+    bool jumpTurn = true;           // jump시 회전을 180도로 제한하기 위한 변수
+    bool dropTurn = true;           // drop시 회전을 90도로 제한하기 위한 변수
+    public float turnSpeed = 500f;  // 회전 모션 속도
+    float rot = 0f;                 // jump에서 공중회전 각도 누적 변수
+    
     GameObject MotionCube;
+
+    // Player(Locket) 변수
+    float AntiGravity = 8.0f;       // Rocket Moded에서 중력에 반대방향으로 동작하는 힘 (일단 public으로 하고 조절하며 값 알아보기)
 
     // Start is called before the first frame update
     void Start()
     {
         cc = GetComponent<CharacterController>();
-        //dir = Vector3.forward;
+        dir = Vector3.forward;
         MotionCube = GameObject.Find("MotionCube");
+        Mode = ModeState.CUBE;
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("jumpState: "+ jumpState);
-
-        // 바닥과 맞닿아 있는지 검사
-        if (cc.isGrounded)      // 바닥에 붙어있을 때
+        switch (Mode)
         {
-            yVelocity = 0;
-            jumpState = false;
+            case ModeState.CUBE: UpdateCube(); break;
+            case ModeState.ROCKET: UpdateRocket(); break;
         }
-        else                    // 바닥과 떨어져 있을때
-        {
-            // yVelocity에 중력을 누적한다.
-            yVelocity += gravity * Time.deltaTime;
-            // jumpState가 true라면 떠있는 동안 MotinoCube 회전시키기 
-            if (jumpState)
-            {
-                /*
-                Vector3 rot = MotionCube.transform.eulerAngles;
-                rot += 180 * Vector3.right * Time.deltaTime;
-                if (rot.x >= 90) rot = new Vector3(-90,0,0);
-                MotionCube.transform.eulerAngles = rot;
-                */
+    }
 
-                float sep = 180 / Time.deltaTime;
-                rot += sep;
-                MotionCube.transform.rotation = Quaternion.Euler(rot, 0, 0);
+    // Mode: Cube일 때
+    private void UpdateCube()
+    {
+        // 1. 바닥과 접촉여부 검사
+        // 1-1) 바닥과 접촉인 경우
+        if (cc.isGrounded)
+        {
+            yVelocity = 0;      // 중력 누적 X
+            jumpState = false;
+            jumpTurn = false;
+            dropTurn = false;
+            rot = 0;
+            MotionCube.transform.rotation = Quaternion.Euler(0, 0, 0);      //  바닥에 닿아있을 때는 rotation 고정
+        }
+        // 1-2) 바닥과 접촉하지 않은 경우
+        else
+        {
+            yVelocity += gravity * Time.deltaTime;      // yVelocity에 중력 적용
+
+            if (jumpState && !jumpTurn)
+            {
+                JumpTurn();     // jump시, MotionCube 180도 회전
+            }
+            if (!dropTurn)
+            {
+                DropTurn();     // 낙하시, MotionCube 90도 회전
             }
         }
 
-        // space 입력 시 jumpPower를 yVelocity에  적용
+        // 2. jump 기능 구현
+        // : space키 입력 && 점프하고 있는 상황이 아니라면
         if (Input.GetButtonDown("Jump") && !jumpState)
         {
             jumpState = true;
-            yVelocity = jumpPower;
+            yVelocity = jumpPower;      // yVeleocity에 jumpPower 적용
         }
+        dir.y = yVelocity;              // dir.y에 yVelocity 적용
 
-        // dir.y에 yVelocity를 적용한다
-        dir.y = yVelocity;
-
-        // 움직이기
+        // 3. cc에 움직입 적용
         cc.Move(dir * moveSpeed * Time.deltaTime);
-
-        
     }
 
+    // Mode: Player일때 동작
+    private void UpdateRocket()
+    {
+        MotionCube.SetActive(false);
+
+
+    }
+
+    // (Cube) jump시 MotinoCube 180도 회전 모션 함수
+    public void JumpTurn()      
+    {
+        rot += turnSpeed * Time.deltaTime;
+        if (rot < 180) MotionCube.transform.rotation = Quaternion.Euler(rot, 0, 0);
+        else jumpTurn = true;
+    }
+
+    // (Cube) drop시 MotinoCube 90도 회전 모션 함수
+    public void DropTurn()
+    {
+        rot += turnSpeed * Time.deltaTime;
+        if (rot < 90) MotionCube.transform.rotation = Quaternion.Euler(rot, 0, 0);
+        else dropTurn = true;
+    }
+
+    public void Dead()
+    {
+        Destroy(gameObject);
+    }
+
+    // yVelocity 시각화 code
     /*private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
@@ -85,4 +139,6 @@ public class PlayerMove : MonoBehaviour
         Vector3 to = transform.position + Vector3.up * yVelocity;
         Gizmos.DrawLine(from, to);
     }*/
+
+
 }
